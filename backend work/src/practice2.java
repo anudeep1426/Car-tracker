64dafb5194929700b5177b511e9d58acb4bf4669
sridherkumar <sri36677@gmail.com>
2017-08-01 16:52:08 -0500

    hello

--- backend work/src/practice2.java
+++ backend work/src/practice2.java
@@ -4,5 +4,5 @@
 public class practice2 {
 
     public static void main(String [] args){
-        System.out.println("local branchgtjbhtvtv gvfcygvuh  anudeep sridher vytcrf change");
+        System.out.println("fofi");
     }}
