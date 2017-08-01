0b20c50d829a38ad0d4188489cefb60afb45494b
sridherkumar <sri36677@gmail.com>
2017-08-01 15:46:57 -0500

    changes done in local branch file java

---
+++ backend work/src/practice.java
@@ -0,0 +1,9 @@
+package com.sridher.javaPractice;
+
+public class practice {
+
+    public static void main(String [] args)
+    {
+        System.out.println("first java class");
+    }
+}
---
+++ backend work/src/sorting.java
@@ -0,0 +1,8 @@
+
+public class sorting {
+
+    public static void main(String[] args){
+        int[] a={5,3,4,2,6,1};
+       System.out.println(a.length);
+    }
+}
