c9a27569fe31eaef26c5d26fba3aad37d5310413
sridherkumar <sri36677@gmail.com>
2017-08-01 16:33:00 -0500

    changes done in local branch file java edited

--- backend work/src/practice2.java
+++ backend work/src/practice2.java
@@ -4,6 +4,6 @@
 public class practice2 {
 
     public static void main(String [] args){
-        System.out.println("local branchgtjbhtvtv change");
+        System.out.println("local branchgtjbhtvtv  vytcrf change");
     }
 }
